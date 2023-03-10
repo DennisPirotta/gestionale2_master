1301734ac0d5c3c5d12cee0dffd451daf2094f55
Dennis <dennispirotta@gmail.com>
2023-03-10 15:38:51 +0100

    Fixed dark mode

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/scss/app.scss',
+                'resources/scss/loader.scss',
             ],
             refresh: [
                 ...refreshPaths,
